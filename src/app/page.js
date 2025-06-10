9a43fef07ab59c9934d0f74cda68514d88fbc50a
Jonathan Hammond <codefolio.work@gmail.com>
2025-06-10 12:26:59 -0400

    buttons from figma. need to create splash next

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,75 @@
 'use client';
+import Button from '../components/Button/Button';
+import { FaStopwatch } from 'react-icons/fa';
+import { PiSparkleFill } from 'react-icons/pi';
 
 import { useSession } from 'next-auth/react';
-
-/*
- For now, assume first name is the first word in the name property
- returns empty string if no name is provided
-*/
-function getfirstname(fullName) {
-  if (!fullName) {
-    return '';
-  }
-  return fullName.trim().split(' ')[0];
-}
+import getFirstName from '../utils/getFirstName';
 
 export default function Home() {
   const { data: session, status } = useSession();
+
+  // Button handlers
+  function handleStart() {
+    alert('Start button clicked!');
+  }
+  function handleComplete() {
+    alert('Complete button clicked!');
+  }
+  function handleNeutral() {
+    alert('Neutral button clicked!');
+  }
+  function handleTimer() {
+    alert('Timer button clicked!');
+  }
+  function handleRecord() {
+    alert('Record Weights button clicked!');
+  }
+  function handleFinish() {
+    alert('Complete button clicked!');
+  }
+  function handleDashboard() {
+    alert('Dashboard button clicked!');
+  }
+  function handleDay() {
+    alert('Day button clicked!');
+  }
 
   if (status === 'loading') {
     return <h1>Loading...</h1>;
   }
 
   if (status === 'unauthenticated') {
-    return <h1>Not logged in</h1>;
+    return (
+      <div style={{ padding: '0 40px' }}>
+        <Button color="red" label="START" onClick={handleStart} />
+        <Button color="green" label="COMPLETED" onClick={handleComplete} />
+        <Button color="white" label="START" onClick={handleNeutral} />
+        <Button
+          color="blue"
+          label="START"
+          icon={<FaStopwatch />}
+          onClick={handleTimer}
+        />
+        <Button
+          color="blue"
+          label="START SET TIMER"
+          icon={<FaStopwatch />}
+          onClick={handleTimer}
+        />
+        <Button
+          color="blue"
+          label="RECORD WEIGHTS"
+          icon={<PiSparkleFill />}
+          onClick={handleRecord}
+        />
+        <Button color="red" label="COMPLETE" onClick={handleFinish} />
+        <Button color="white" label="DASHBOARD" onClick={handleDashboard} />
+        <Button color="red" label="DAY 4" onClick={handleDay} />
+        <h1>Not logged in</h1>
+      </div>
+    );
   }
 
-  return <h1>Welcome, {getfirstname(session?.user?.name)}</h1>;
+  return <h1>Welcome, {getFirstName(session?.user?.name)}</h1>;
 }
---
+++ src/utils/getFirstName.js
@@ -0,0 +1,12 @@
+/*
+ For now, assume first name is the first word in the name property
+ returns empty string if no name is provided
+*/
+function getFirstName(fullName) {
+  if (!fullName) {
+    return '';
+  }
+  return fullName.trim().split(' ')[0];
+}
+
+export default getFirstName;
